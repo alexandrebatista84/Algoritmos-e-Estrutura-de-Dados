24e2dd5f5c1ddf91e14f3f64abaa3658d912b243
Alexandre Batista <alexandrebatista84@gmail.com>
2018-02-22 16:23:04 +0000

    hello.c

---
+++ prog0207.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+{
+	float raio, perimetro;
+	double pi=3.1415927, area;
+	
+	printf("Introduza o raio da circunferência: ");
+	scanf("%f",&raio);
+	
+	perimetro=2*pi*raio;
+	area=pi*raio*raio;
+	
+	printf("O perimetro da circunferência é %f e a sua área é %f.\n",perimetro,area);
+	
+}
