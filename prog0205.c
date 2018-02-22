9daf7ff301c696ae548e2deb4e58a3d844de3257
Alexandre Batista <alexandrebatista84@gmail.com>
2018-02-22 15:24:17 +0000

    hello.c

---
+++ prog0205.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("O tamanho em bytes de um char é %d.\n",sizeof(char));
+	printf("O tamanho em bytes de um inteiro é %d.\n",sizeof(short int));
+	printf("O tamanho em bytes de um float é %d.\n",sizeof(float));
+	printf("O tamanho em bytes de um double é %d.\n",sizeof(double));
+	return(0);
+}
